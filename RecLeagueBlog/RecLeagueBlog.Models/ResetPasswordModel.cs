b23f9ffef7871414e4b221b5bef91a2e18e2903e
Alex Cooley <alexdcooley@gmail.com>
2017-10-31 09:28:24 -0500

    password reset ui

---
+++ RecLeagueBlog/RecLeagueBlog.Models/ResetPasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecLeagueBlog.Models
+{
+    public class ResetPasswordModel
+    {
+        //[Required]
+        //public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        //[Required]
+        //public string Token { get; set; }
+    }
+}
