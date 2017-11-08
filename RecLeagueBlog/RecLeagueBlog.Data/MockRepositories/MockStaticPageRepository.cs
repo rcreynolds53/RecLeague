790fb6061fd05e0ae292d16189c07565700e2163
Alex Cooley <alexdcooley@gmail.com>
2017-11-08 10:14:37 -0600

    static page push

--- RecLeagueBlog/RecLeagueBlog.Data/MockRepositories/MockStaticPageRepository.cs
+++ RecLeagueBlog/RecLeagueBlog.Data/MockRepositories/MockStaticPageRepository.cs
@@ -10,29 +10,39 @@ namespace RecLeagueBlog.Data.MockRepositories
 {
     public class MockStaticPageRepository : IStaticPageRepository
     {
+        private static List<StaticPage> _pages;
         public void CreateStaticPage(StaticPage newPage)
         {
-            throw new NotImplementedException();
+            if (_pages.Any())
+            {
+                newPage.StaticPageId = _pages.Max(p => p.StaticPageId) + 1;
+            }
+            else
+            {
+                newPage.StaticPageId = 1;
+            }
+            _pages.Add(newPage);
         }
 
         public void DeleteStaticPage(int staticPageId)
         {
-            throw new NotImplementedException();
+            _pages.RemoveAll(p => p.StaticPageId == staticPageId);
         }
 
         public void EditStaticPage(StaticPage updatedPage)
         {
-            throw new NotImplementedException();
+            _pages.RemoveAll(p => p.StaticPageId == updatedPage.StaticPageId);
+            _pages.Add(updatedPage);
         }
 
         public List<StaticPage> GetAllPages()
         {
-            throw new NotImplementedException();
+            return _pages.OrderBy(p => p.StaticPageId).ToList();
         }
 
         public StaticPage GetPageByID(int staticPageId)
         {
-            throw new NotImplementedException();
+            return _pages.FirstOrDefault(p => p.StaticPageId == staticPageId);
         }
     }
 }
